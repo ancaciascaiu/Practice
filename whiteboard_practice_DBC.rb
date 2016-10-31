7b5fcf88d2078e03326c17ba9b07c19da372f3b9
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-10-31 17:44:37 -0500

    last changes before presentation

--- whiteboard_practice_DBC.rb
+++ whiteboard_practice_DBC.rb
@@ -4,12 +4,13 @@ def reverse(array)
 	length = array.length
 	n = 1
 	array.each_with_index do |element, index|
-		this_element = array[position]
-		array[position] = array[length-1]
+		this_element = array[index]
+		array[index] = array[length-1]
 		array[length-1] = this_element
 		n += 1 # where does it need to stop?
 	end
 	return array
+
 end
 
-reverse(["a", "b", "c", "d", "e"])
+p reverse(["a", "b", "c", "d", "e"])
