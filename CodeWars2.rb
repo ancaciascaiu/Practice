7f227d9321372ab276176109aefd15ec58938b32
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-02 11:36:43 -0500

    code works for an array of integers by sorting odd and even numbers and counting them

--- CodeWars2.rb
+++ CodeWars2.rb
@@ -6,9 +6,27 @@
 
 def find_outlier(integers)
   even_nr_enumerator = 0
+  even_nr = 0
   odd_nr_enumerator = 0
+  odd_nr = 0
 
-  
+  integers.each do |integer|
+  	if integer % 2 == 0
+  		even_nr = integer
+  		even_nr_enumerator += 1
+  	else
+  		odd_nr = integer
+  		odd_nr_enumerator += 1
+  	end
+  end
+
+  if even_nr_enumerator == 1
+  	return even_nr
+  elsif odd_nr_enumerator == 1
+  	return odd_nr
+  else
+  	return "There is no outlier in this collection of numbers"
+  end
 end
 
 p find_outlier([0, 1, 2])
