5a73d8f617419e8ee9ec2fd1ec0ad14188a65922
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-02 11:08:30 -0500

    setup method for getting the outlier number out of an array

---
+++ CodeWars2.rb
@@ -0,0 +1,16 @@
+# You are given an array (which will have a length of at least 3,
+#  but could be very large) containing integers. The array is 
+# either entirely comprised of odd integers or entirely 
+# comprised of even integers except for a single integer N. 
+# Write a method that takes the array as an argument and returns N.
+
+def find_outlier(integers)
+  even_nr_enumerator = 0
+  odd_nr_enumerator = 0
+
+  
+end
+
+p find_outlier([0, 1, 2])
+p find_outlier([1, 2, 3])
+p find_outlier([2,6,8,10,3])
