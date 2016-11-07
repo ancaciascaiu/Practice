c0222fb18c4dd254a6311ba32a8fcde36ced90f0
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-07 10:29:55 -0600

    order of 1, constant time

--- big-o.rb
+++ big-o.rb
@@ -4,6 +4,7 @@
 # it's a linear graph(straight line). If we graph the time it takes to run this function for 
 # a 3 item array/ a 10,000 item array, we'd see that the time corresponds to the 
 # number of items in the array:
+# i.e. The further away in the list the number we're looking for, the longer it takes to find it.
 def item_in_list(to_check, the_list)
 	the_list.each do |element|
 		if to_check == element
@@ -13,6 +14,38 @@ def item_in_list(to_check, the_list)
 	false
 end
 
-p item_in_list(3, [1, 2, 3])
+item_in_list(3, [1, 2, 3])
+
+# O(1) = called "constant time". 
+# no matter how big our input is, it always takes the same amount of time to compute things.
+
+def all_combinations(the_list)
+	results = []
+	the_list.each do |item|
+		the_list.each do |inner_item|
+			results.push([item, inner_item])
+		end
+	end
+	return results
+end
+
+p all_combinations([1, 2, 3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
