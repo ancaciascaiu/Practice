9242dbc5ab7a79525bb52d6588878e4bd2a82c92
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-10 15:35:37 -0600

    add logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,11 +5,25 @@
 
 def bubble_sort(array)
 	#outer loop keeps track if something was swapped or not
-	array.each do |number|
-		swapped = false;
+	swapped = false;
+	until swapped == true do 
 		#inner loop swaps numbers that aren't in order
-		array.each do
+		array.each_with_index do |number, index|
 			#take each pair of numbers and compare them. Swap if a > b. If so, swapped = true.
+			p array[index]
+			p array[index + 1]
+			p "@@@@"
+			
+			if index != array.length-1
+				if array[index] > array[index + 1]
+					array[index] == array[index + 1]
+					array[index + 1] == array[index]
+					swapped = true
+					p array
+				end
+			end
 		end
 	end
 end
+
+p bubble_sort([1,2,3,6,4,8,5])
