cd06bd5b82d680f423a10b7e7f87ff77baeb05be
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-14 18:29:24 -0600

    first commit merge sort. Add definition

---
+++ merge_sort.rb
@@ -0,0 +1,8 @@
+# An element is compared to all the prior elements 
+# until a lesser element is found. If an element contains 
+# a value less than all the previous elements, it compares 
+# the element to all the previous elements before going on 
+# to the next comparison
+
+
+
