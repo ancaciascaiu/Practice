efaaa6422fcc83e5f9a364e4014f350954ddb1ba
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-14 18:02:34 -0600

    execution works partially. Needs debugging

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -8,7 +8,15 @@ def insertion_sort(array)
 	# outer loop iterates through all elements of the array
 	array.each_with_index do |val, i|
 		# inner loop compares val to previous val until previous val < val
+		j = i - 1 # previous index
+		while j >= 0
+			break if array[i] > array[j]
+			array[i] = array[j]
+			j -= 1
+		end
+		array[j + 1] = val
 	end
+	array
 end
 
 p insertion_sort([1,5,8,2,4,9,3,7,0,6])
