4151b0a17ccdd67498779789541d7452c6b001ab
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-14 19:58:49 -0600

    version 1: method style

---
+++ business_card.rb
@@ -0,0 +1,9 @@
+def anca_fodor_ciascaiu(info = {})
+	# web developer
+	info["callme"] = "773-501-2982"
+	info["emailme"] = "anca.ciascaiu@gmail.com"
+	info["linkedin"] = "/in/ancafodorciascaiu"
+	puts info
+end
+
+anca_fodor_ciascaiu()
