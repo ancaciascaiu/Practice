54acc8b848e57e2fc0d50b5ac57e2501032a0fde
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-27 14:40:20 -0600

    iterative solution works

--- palindrome_algorithm.rb
+++ palindrome_algorithm.rb
@@ -4,8 +4,14 @@
 
 #iterative solution
 def is_palindrome?(array)
+	n = 0
 	0..(array.length.to_f/2).ceil.times do |element|
-		
+		if array[n] == array[-(n+1)]
+			this_element = true
+		else
+			return false
+		end
+		return this_element
 	end
 end
 
@@ -13,7 +19,7 @@ end
 #recursive solution
 
 
-is_palindrome?([1,2,3,4,3,2,1]) #=>true
-is_palindrome?([5,6,7,7,6,5]) #=>true
-is_palindrome?([7,8,9,0,9,8,7,6]) #=>false
-is_palindrome?([1,2,3,3,2])
+p is_palindrome?([1,2,3,4,3,2,1]) #=>true
+p is_palindrome?([5,6,7,7,6,5]) #=>true
+p is_palindrome?([7,8,9,0,9,8,7,6]) #=>false
+p is_palindrome?([1,2,3,3,2]) #=>false
