5a89b19b40cf02a91a0c1c4698aeb1971fff8c02
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-11-27 15:49:56 -0600

    iterative version works for all examples

--- palindrome_algorithm.rb
+++ palindrome_algorithm.rb
@@ -5,21 +5,31 @@
 #iterative solution
 def is_palindrome?(array)
 	n = 0
-	0..(array.length.to_f/2).ceil.times do |element|
-		if array[n] == array[-(n+1)]
-			this_element = true
-		else
+	length = array.length
+	0..(length.to_f/2).ceil.times do |element|
+		if array[n] != array[-(n+1)]
 			return false
 		end
-		return this_element
+		n += 1
 	end
+	return true
 end
 
 
-#recursive solution
+recursive solution
+def recursive_palindrome?(array)
+	n = 0
+	if array[n] == array[-(n+1)]
+	end
+end
 
 
 p is_palindrome?([1,2,3,4,3,2,1]) #=>true
 p is_palindrome?([5,6,7,7,6,5]) #=>true
 p is_palindrome?([7,8,9,0,9,8,7,6]) #=>false
-p is_palindrome?([1,2,3,3,2]) #=>false
+p is_palindrome?([1,2,3,3,2,5,1]) #=>false
+
+p recursive_palindrome?([1,2,3,4,3,2,1]) #=>true
+p recursive_palindrome?([5,6,7,7,6,5]) #=>true
+p recursive_palindrome?([7,8,9,0,9,8,7,6]) #=>false
+p recursive_palindrome?([1,2,3,3,2]) #=>false
