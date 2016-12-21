9d1bdaee914ecc78accd885ccc4e1814355e8bcb
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-12-20 19:04:59 -0600

    returns false or true correctly

--- unique_chars.rb
+++ unique_chars.rb
@@ -1,7 +1,17 @@
 #Write an algorithm to determine if the characters in a string are unique
 
 def unique?(string)
-	
+	unique_chars = {}
+	string.each_char do |char|
+		if char == " "
+			#next
+		elsif unique_chars[char]
+			return false
+		elsif !unique_chars[char]
+			unique_chars[char] = 1
+		end
+	end
+	true
 end
 
 p unique?("Anca has a string.") #=>returns false
