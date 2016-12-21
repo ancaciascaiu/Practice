5ae34f7d6bd0b6356eba18adba960e91e459da21
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-12-20 18:56:20 -0600

    set up method to check if all chars in a string are unique

---
+++ unique_chars.rb
@@ -0,0 +1,8 @@
+#Write an algorithm to determine if the characters in a string are unique
+
+def unique?(string)
+	
+end
+
+p unique?("Anca has a string.") #=>returns false
+p unique?("Love ya!") #=>returns true
