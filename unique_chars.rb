b6bfad4f51080c9ca180d66b91ab16fc10d62c47
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-12-20 19:10:18 -0600

    consider edge case where we dont want to count spaces

--- unique_chars.rb
+++ unique_chars.rb
@@ -4,15 +4,16 @@ def unique?(string)
 	unique_chars = {}
 	string.each_char do |char|
 		if char == " "
-			#next
+			next
 		elsif unique_chars[char]
 			return false
 		elsif !unique_chars[char]
 			unique_chars[char] = 1
 		end
+		p char
 	end
 	true
 end
 
 p unique?("Anca has a string.") #=>returns false
-p unique?("Love ya!") #=>returns true
+p unique?("Luv ya friend! ") #=>returns true
