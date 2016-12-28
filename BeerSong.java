ba65686c3b155c2721403227e01f579ef49c47df
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-12-28 16:20:12 -0600

    print verse inline, compile

---
+++ BeerSong.java
@@ -0,0 +1,26 @@
+public class BeerSong {
+	public static void main (String[] args) {
+		int beerNum = 99;
+		String word = "bottles";
+
+		while (beerNum > 0) {
+			if (beerNum == 1) {
+				word = "bottle";
+			}
+
+		System.out.print(beerNum + " " + word + " of beer on the wall, ");
+		System.out.println(beerNum + " " + word + " of beer.");
+		System.out.print("Take one down and pass it around, ");
+		
+		beerNum = beerNum - 1;
+
+		if (beerNum > 0) {
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+
+		} else {
+			System.out.println("no more bottles of beer on the wall");
+
+		}
+		}
+	}
+}
