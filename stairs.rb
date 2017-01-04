72c5f7c0955c9f875975f19d42a59dcb02963072
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-01-04 16:40:57 -0600

    code prints correctly but inline

--- stairs.rb
+++ stairs.rb
@@ -5,4 +5,15 @@
 #   *
 #  *
 # *
+# it will take an argument, n, for how many stairs we want to build
 
+def stairs(n)
+	text_to_print = ""
+	n.times do |line|
+		text_to_print += (" " * (n-1)) + ("*" * (n-(n-1))) 
+		n -= 1
+	end
+	text_to_print
+end
+
+p stairs(6)
