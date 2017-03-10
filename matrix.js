22122f5f3832c6211aa92eb5e208090d60dea88e
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-03-10 15:26:20 -0600

    correct details

--- matrix.js
+++ matrix.js
@@ -9,12 +9,19 @@
 
 var setZeros = function(matrix){
 	//to remember which columns we need to fill with 1's
+
+	if(matrix.length == 0)
+		return;
+
 	var rows = [];
 	var columns = [];
+
+
+
 	for(i=0; i<(matrix.length); i++){
-		var rowLength = matrix[i].length
+		var colLength = matrix[i].length
 		// console.log(matrix[i])
-		for(j=0; j<(matrix[i].length); j++){
+		for(j=0; j<colLength; j++){
 			// console.log(matrix[i][j])
 			if(matrix[i][j]==1){
 				//if element is 1, note which row and column we need to fill with 1's
@@ -28,12 +35,12 @@ var setZeros = function(matrix){
 	console.log("Columns:", columns)
 
 	//fill matrix with 1's on rows and columns
-	for(i=0; i<(rows.length); i++){
+	for(i=0; i<rows.length; i++){
 		matrix[rows[i]].fill(1)
 	}
 
-	for(i=0; i<(matrix.length); i++){
-		for(j = 0; j<(matrix[i].length); j++){
+	for(i=0; i<matrix.length; i++){
+		for(j = 0; j<matrix[i].length; j++){
 			if(columns.includes(j)){
 				matrix[i][j] = 1
 			}
@@ -43,5 +50,5 @@ var setZeros = function(matrix){
 	console.log(matrix)
 }
 
-setZeros([[1,0,0,1], [0,0,1,0], [0,0,0,0]])
+setZeros([[1,0,0,1], [0,0,1,0], [0,0,0,0]]) //<-returns [[ 1, 1, 1, 1 ], [ 1, 1, 1, 1 ], [ 1, 0, 1, 1 ]]
 
