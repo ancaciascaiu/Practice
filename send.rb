908b5a2780942f6c43261e1ceee5547ca90c3432
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-04-05 11:25:17 +0300

    Sender will connect to RabbitMQ, send a single message, then exit.

--- send.rb
+++ send.rb
@@ -1,3 +1,15 @@
 #sender for RabbitMQ
+#The sender will connect to RabbitMQ, send a single message, then exit.
+require "bunny"
 
+#connect to RabbitMQ server
+conn = Bunny.new(:automatically_recover => false)
+conn.start
+#create a channel, which is where most of the API for getting things done resides:
+ch = conn.create_channel
+# To send, we must declare a queue for us to send to; then we can publish a message to the queue:
+q = ch.queue("hello")
+ch.default_exchange.publish("Hello World!", :routing_key => q.name)
+puts " [x] Sent 'Hello World!'"
 
+conn.close
