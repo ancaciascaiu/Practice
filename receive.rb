1e24d590404dc8f30d9196ba397649662394a4fb
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-04-06 23:10:46 +0300

    send and receive messages from a named queue

--- receive.rb
+++ receive.rb
@@ -9,15 +9,19 @@ conn = Bunny.new(:automatically_recover => false)
 conn.start
 
 ch = conn.create_channel
-q  = ch.queue("hello")
+q  = ch.queue("hell")
 
 begin
-	#Note that we declare the queue here, as well. Because we might start the consumer before the producer, we want to make sure the queue exists before we try to consume messages from it.
-	#We're about to tell the server to deliver us the messages from the queue. Since it will push us messages asynchronously, we provide a callback that will be executed when RabbitMQ pushes messages to our consumer. This is what Bunny::Queue#subscribe does.
+	#Note that we declare the queue here, as well. Because we might 
+	# start the consumer before the producer, we want to make sure the 
+	# queue exists before we try to consume messages from it.
+	#We're about to tell the server to deliver us the messages from 
+	# the queue. Since it will push us messages asynchronously, 
+	# we provide a callback that will be executed when RabbitMQ pushes 
+	# messages to our consumer. This is what Bunny::Queue#subscribe does.
 	puts " [*] Waiting for messages in #{q.name}. To exit press CTRL+C"
 	q.subscribe(:block => true) do |delivery_info, properties, body|
 	  puts " [x] Received #{body}"
-
 	  # cancel the consumer to exit
 	  # delivery_info.consumer.cancel
 	end
--- send.rb
+++ send.rb
@@ -1,4 +1,5 @@
 #sender for RabbitMQ
+#!!!send and receive messages from a named queue
 #The sender will connect to RabbitMQ, send a single message, then exit.
 require "bunny"
 
@@ -8,8 +9,7 @@ conn.start
 #create a channel, which is where most of the API for getting things done resides:
 ch = conn.create_channel
 # To send, we must declare a queue for us to send to; then we can publish a message to the queue:
-q = ch.queue("hello")
+q = ch.queue("hell")
 ch.default_exchange.publish("Hello World!", :routing_key => q.name)
 puts " [x] Sent 'Hello World!'"
-
 conn.close
