89fe71aa083c02626f85f454f1ad9af0e3c6e945
ancaciascaiu <anca.ciascaiu@gmail.com>
2017-10-08 12:42:50 -0500

    add rim and tire instance variables to calculate gear_inches

--- poodr.rb
+++ poodr.rb
@@ -4,18 +4,24 @@
 
 # Nouns are great candidates for classes: bike, gear
 class Gear
-    attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-    def initialize(chainring, cog)
-        @chainring = chainring
-        @cog = cog
-    end
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
 
-    def ratio
-        chainring / cog.to_f
-    end
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    #tire goes around rim twice for diameter
+    ratio * (rim + (tire * 2))
+  end
 end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
-
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
