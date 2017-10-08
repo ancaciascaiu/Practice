4d77e5ee4da3d7c0b942a416ed34e2d519a17bb8
ancaciascaiu <anca.ciascaiu@gmail.com>
2017-10-08 13:23:07 -0500

    the real deal. Separate into Gear class and Wheel class

--- poodr.rb
+++ poodr.rb
@@ -6,10 +6,10 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -19,12 +19,27 @@ class Gear
   def gear_inches
     ratio * wheel.diameter
   end
+end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
+wheel = Wheel.new(26, 1.5)
+puts wheel.circumference
+
+gear = Gear.new(52, 11, wheel)
+puts gear.gear_inches
+puts gear.ratio
