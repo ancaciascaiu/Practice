f1d04bbaa7449304f2053ae5f219a49877ee508f
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-02-17 14:18:11 -0600

    accomodate 2 types of bikes

--- poodr.rb
+++ poodr.rb
@@ -63,19 +63,28 @@ class Mechanic
 end
 
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
 
   def initialize(args)
-    @size = args[:size]
-    @tape_color = args[:tape_color]
+    @style       = args[:style]
+    @size        = args[:size]
+    @tape_color  = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
   end
 
   def spares
-    {chain: '10-speed',
-     tire_size: '23',
-     tape_color: tape_color}
+    if style == :road
+      {chain:      '10-speed',
+       tire_size:  '23',
+       tape_color: tape_color}
+    else
+      {chain:      '10-speed',
+       tire_size:  '2.1',
+       rear_shock: rear_shock}
+    end
   end
 end
 
-bike = Bicycle.new(size: 'M', tape_color: 'red')
+bike = Bicycle.new(style: :mountain, size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
 p bike.spares
