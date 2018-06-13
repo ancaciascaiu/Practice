b66c1c5536e3a67042aba7e44cb8e0feeadae552
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-06-13 14:42:07 -0500

    move all private methods' logic into 'verse'. Refactor the code for readability

--- 99bottles-take1/lib/bottles.rb
+++ 99bottles-take1/lib/bottles.rb
@@ -11,30 +11,19 @@ class Bottles
   end
 
   def verse(number_of_bottles)
-    first_part(number_of_bottles) + "\n" + second_part(number_of_bottles) + "\n"
-  end
-
-  private
-
-  def first_part(number_of_bottles)
-    pre_bottles = number_of_bottles.zero? ? 'no more' : number_of_bottles.to_s
-    "#{pre_bottles.capitalize} #{bottle_str(number_of_bottles)} of beer on the wall, " +
-      "#{pre_bottles} #{bottle_str(number_of_bottles)} of beer."
-  end
-
-  def second_part(number_of_bottles)
-    number_of_bottles_left = number_of_bottles - 1
-
     if number_of_bottles.zero?
-      'Go to the store and buy some more, 99 bottles of beer on the wall.'
+      first_part  = 'No more bottles of beer on the wall, no more bottles of beer.'
+      second_part = 'Go to the store and buy some more, 99 bottles of beer on the wall.'
     elsif number_of_bottles == 1
-      'Take it down and pass it around, no more bottles of beer on the wall.'
+      first_part  = "#{number_of_bottles} bottle of beer on the wall, #{number_of_bottles} bottle of beer."
+      second_part = 'Take it down and pass it around, no more bottles of beer on the wall.'
     else
-      "Take one down and pass it around, #{number_of_bottles_left} #{bottle_str(number_of_bottles_left)} of beer on the wall."
+      bottle_str = number_of_bottles == 2 ? 'bottle' : 'bottles'
+      first_part  = "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
+      second_part = 'Take one down and pass it around, ' \
+                    "#{number_of_bottles - 1} #{bottle_str} of beer on the wall."
     end
-  end
 
-  def bottle_str(number_of_bottles)
-    number_of_bottles == 1 ? 'bottle' : 'bottles'
+    first_part + "\n" + second_part + "\n"
   end
 end
