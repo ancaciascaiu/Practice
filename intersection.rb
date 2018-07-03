2f02066db4535ea509a73ee0ca0d5b78458d848e
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-07-03 11:40:43 -0500

    infinite loop to switch colors in tandem

--- intersection.rb
+++ intersection.rb
@@ -2,13 +2,53 @@
 # change colors in this order: Green-Yellow-Red.
 
 class Intersection
+  attr_reader :stoplight1, :stoplight2
+
   def initialize
     @stoplight1 = StopLight.new
     @stoplight2 = StopLight.new
   end
+
+  def run
+    loop do
+      # to start
+      if stoplight1.red && stoplight2.red
+        stoplight1.red = false
+        stoplight1.green = true
+        p stoplight1
+        p stoplight2
+      elsif stoplight1.green
+        stoplight1.green = false
+        stoplight1.yellow = true
+        p stoplight1
+        p stoplight2
+        # wait
+        stoplight1.yellow = false
+        stoplight1.red = true
+        stoplight2.red = false
+        stoplight2.green = true
+        p stoplight1
+        p stoplight2
+      elsif stoplight2.green
+        stoplight2.green = false
+        stoplight2.yellow = true
+        p stoplight1
+        p stoplight2
+        # wait
+        stoplight2.yellow = false
+        stoplight2.red = true
+        stoplight1.red = false
+        stoplight1.green = true
+        p stoplight1
+        p stoplight2
+      end
+    end
+  end
 end
 
 class StopLight
+  attr_accessor :red, :yellow, :green
+
   def initialize
     @red    = true
     @yellow = false
@@ -16,4 +56,4 @@ class StopLight
   end
 end
 
-p Intersection.new
+Intersection.new.run
