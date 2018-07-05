0a16e1f65f26953d67ba9b93653b5cbd6992958d
ancaciascaiu <anca.ciascaiu@gmail.com>
2018-07-05 09:32:16 -0500

    add sleep time

--- intersection.rb
+++ intersection.rb
@@ -16,25 +16,31 @@ class Intersection
         stoplight1.yellow = true
         p stoplight1
         p stoplight2
-        # wait
+        sleep(15)
+        p "----------------"
         stoplight1.yellow = false
         stoplight1.red = true
         stoplight2.red = false
         stoplight2.green = true
         p stoplight1
         p stoplight2
+        sleep(15)
+        p "----------------"
       elsif stoplight2.green
         stoplight2.green = false
         stoplight2.yellow = true
         p stoplight1
         p stoplight2
-        # wait
+        sleep(15)
+        p "----------------"
         stoplight2.yellow = false
         stoplight2.red = true
         stoplight1.red = false
         stoplight1.green = true
         p stoplight1
         p stoplight2
+        sleep(15)
+        p "----------------"
       end
     end
   end
@@ -44,9 +50,9 @@ class StopLight
   attr_accessor :red, :yellow, :green
 
   def initialize(red:, yellow:, green:)
-    @red    = true
-    @yellow = false
-    @green  = false
+    @red    = red
+    @yellow = yellow
+    @green  = green
   end
 end
 
