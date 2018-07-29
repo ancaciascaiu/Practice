f0935f2de5db73c67498b3a1ed34c3fd84219a91
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2018-07-29 18:01:20 -0500

    prototype for simple, chevron and triangle bracelets

---
+++ bracelets.rb
@@ -0,0 +1,26 @@
+# simple bracelet:(min 2 colors, min 4 threads, any nr of threads)
+# knots:
+puts "X__"
+puts "OX_"
+puts "XOX"
+puts "OXO"
+puts "_OX"
+puts "__O"
+
+# chevron bracelet: (min 2 colors, min 4 threads, even nr of threads, half mirroring the other half)
+# knots:
+puts "X_X"
+puts "OXO"
+puts "XOX"
+puts "_X_"
+
+# triangle bracelet:(min 2 colors, min 4 threads)
+# knots:
+puts "X__"
+puts "XX_"
+puts "XXO"
+puts "XOX"
+puts "OXX"
+puts "XOX"
+puts "_XO"
+puts "__X"
